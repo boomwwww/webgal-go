36cd384bf4bb6a22e2d8b39abaee8d408a20114b
boomwwww <boom_w_w@outlook.com>
2025-10-08 23:36:34 +0800

    chore(root): any

--- eslint.config.js
+++ eslint.config.js
@@ -16,68 +16,100 @@ const ignores = [
   '**/public/webgal*',
 ]
 
-// export default [
-//   {
-//     ignores,
-//     ...eslint.configs.recommended,
-//     ...tseslint.configs.recommended,
-//     ...eslintConfigPrettier,
-//     plugins: {
-//       prettier: eslintPluginPrettier,
-//     },
-//     languageOptions: {
-//       ecmaVersion: 'latest',
-//       sourceType: 'module',
-//       parser: tseslint.parser,
-//     },
-//     rules: {
-//       'no-var': 'error',
-//       // todo
-//     },
-//   },
-//   // node
-//   // {
-//   //   ignores,
-//   //   files: [],
-//   //   languageOptions: {
-//   //     globals: {
-//   //       ...globals.browser,
-//   //     },
-//   //   },
-//   // },
-//   // vue
-//   {
-//     ignores,
-//     ...eslintPluginVue.configs['flat/recommended'],
-//     ...eslintConfigPrettier,
-//     files: ['playgrounds/vite-vue/**/*.{ts,js,tsx,jsx,vue}'],
-//     languageOptions: {
-//       globals: {
-//         ...globals.browser,
-//       },
-//     },
-//   },
-//   // react
-//   {
-//     ignores,
-//     ...eslintPluginReact.configs.flat,
-//     ...eslintConfigPrettier,
-//     files: ['playgrounds/vite-react/**/*.{ts,js,tsx,jsx}'],
-//     languageOptions: {
-//       globals: {
-//         ...globals.browser,
-//       },
-//     },
-//   },
-// ]
-export default [
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist', ...ignores]),
   {
-    ignores, // 忽略目录
+    files: ['playgrounds/vite-react/**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
   },
   {
     rules: {
-      'no-console': 'error', // 禁止使用 console
-      'no-var': 'error',
+      semi: ['error', 'never'], // 禁止分号
     },
   },
-]
+])
+
+// export default [
+// {
+//   ignores,
+//   ...eslint.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   ...eslintConfigPrettier,
+//   plugins: {
+//     prettier: eslintPluginPrettier,
+//   },
+//   languageOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//     parser: tseslint.parser,
+//   },
+//   rules: {
+//     'no-var': 'error',
+//     // todo
+//   },
+// },
+// node
+// {
+//   ignores,
+//   files: [],
+//   languageOptions: {
+//     globals: {
+//       ...globals.browser,
+//     },
+//   },
+// },
+// vue
+//   {
+//     files: ['playgrounds/vite-vue/**/*.{ts,js,tsx,jsx,vue}'],
+//     ignores,
+//     ...eslintPluginVue.configs['flat/recommended'],
+//     ...eslintConfigPrettier,
+
+//     languageOptions: {
+//       globals: {
+//         ...globals.browser,
+//       },
+
+//     },
+//     plugins: {
+//       vue: eslintPluginVue,
+//     },
+//   },
+// react
+// {
+//   ignores,
+//   ...eslintPluginReact.configs.flat,
+//   ...eslintConfigPrettier,
+//   files: ['playgrounds/vite-react/**/*.{ts,js,tsx,jsx}'],
+//   languageOptions: {
+//     globals: {
+//       ...globals.browser,
+//     },
+//   },
+// },
+// ]
+// export default [
+//   {
+//     ignores, // 忽略目录
+//     rules: {
+//       'no-console': 'error', // 禁止使用 console
+//       'no-var': 'error',
+//     },
+//   },
+// ]
